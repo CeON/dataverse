8ed179a74574a56b13808cff1f491145616517b1
Krzysztof Mądry <madryk@icm.edu.pl>
2019-08-05 12:25:18 +0200

    Closes #235: cr response

--- dataverse-persistence/src/main/java/edu/harvard/iq/dataverse/common/FriendlyFileTypeUtil.java
+++ dataverse-persistence/src/main/java/edu/harvard/iq/dataverse/common/FriendlyFileTypeUtil.java
@@ -2,6 +2,7 @@ package edu.harvard.iq.dataverse.common;
 
 import edu.harvard.iq.dataverse.common.files.mime.ShapefileMimeType;
 import edu.harvard.iq.dataverse.persistence.datafile.DataFile;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.MissingResourceException;
 
@@ -10,21 +11,17 @@ public class FriendlyFileTypeUtil {
     public static String getUserFriendlyFileType(DataFile dataFile) {
         String fileType = dataFile.getContentType();
 
-        if (fileType != null) {
-            if (fileType.equalsIgnoreCase(ShapefileMimeType.SHAPEFILE_FILE_TYPE.getMimeValue())) {
-                return ShapefileMimeType.SHAPEFILE_FILE_TYPE.getFriendlyName();
-            }
-            if (fileType.contains(";")) {
-                fileType = fileType.substring(0, fileType.indexOf(";"));
-            }
-            try {
-                return BundleUtil.getStringFromPropertyFile(fileType, "MimeTypeDisplay");
-            } catch (MissingResourceException e) {
-                return fileType;
-            }
+        if (fileType.equalsIgnoreCase(ShapefileMimeType.SHAPEFILE_FILE_TYPE.getMimeValue())) {
+            return ShapefileMimeType.SHAPEFILE_FILE_TYPE.getFriendlyName();
         }
-
-        return fileType;
+        if (fileType.contains(";")) {
+            fileType = fileType.substring(0, fileType.indexOf(";"));
+        }
+        try {
+            return BundleUtil.getStringFromPropertyFile(fileType, "MimeTypeDisplay");
+        } catch (MissingResourceException e) {
+            return fileType;
+        }
     }
     
     
@@ -35,7 +32,7 @@ public class FriendlyFileTypeUtil {
 
         String fileType = dataFile.getOriginalFileFormat();
 
-        if (fileType != null && !fileType.equals("")) {
+        if (StringUtils.isNotEmpty(fileType)) {
             if (fileType.contains(";")) {
                 fileType = fileType.substring(0, fileType.indexOf(";"));
             }
