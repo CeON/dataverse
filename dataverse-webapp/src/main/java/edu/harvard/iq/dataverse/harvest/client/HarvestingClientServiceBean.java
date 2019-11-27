fef868020c7a4807243e03d298f9ddaddd991bac
dbojanek <dbojanek@icm.edu.pl>
2019-11-27 08:57:53 +0100

    Closes #482: HarvestingClientsService (commands operations)

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/harvest/client/HarvestingClientServiceBean.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/harvest/client/HarvestingClientServiceBean.java
@@ -4,13 +4,11 @@ import edu.harvard.iq.dataverse.DataFileServiceBean;
 import edu.harvard.iq.dataverse.DataverseDao;
 import edu.harvard.iq.dataverse.DataverseRequestServiceBean;
 import edu.harvard.iq.dataverse.EjbDataverseEngine;
-import edu.harvard.iq.dataverse.persistence.datafile.DataFile;
 import edu.harvard.iq.dataverse.persistence.harvest.ClientHarvestRun;
 import edu.harvard.iq.dataverse.persistence.harvest.HarvestingClient;
 import edu.harvard.iq.dataverse.search.IndexServiceBean;
 import edu.harvard.iq.dataverse.timer.DataverseTimerServiceBean;
 
-import javax.ejb.Asynchronous;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
@@ -44,6 +42,8 @@ public class HarvestingClientServiceBean implements java.io.Serializable {
     IndexServiceBean indexService;
     @EJB
     DataverseTimerServiceBean dataverseTimerService;
+    @Inject
+    private HarvestingClientsService harvestingClientsService;
 
     @PersistenceContext(unitName = "VDCNet-ejbPU")
     private EntityManager em;
@@ -118,49 +118,6 @@ public class HarvestingClientServiceBean implements java.io.Serializable {
         }
         em.refresh(harvestingClient); // why are we doing this?
         harvestingClient.setDeleteInProgress(true);
-    }
-
-    // Deleting a client, with all the associated content, can take a while - 
-    // hence it's an async action: 
-    // TOFIGUREOUT:
-    // for whatever reason I cannot call the DeleteHarvestingClientCommand from
-    // inside this method; something to do with it being asynchronous?
-    @Asynchronous
-    public void deleteClient(Long clientId) {
-        String errorMessage = null;
-        HarvestingClient victim = find(clientId);
-
-        if (victim == null) {
-            return;
-        }
-
-        try {
-            //engineService.submit(new DeleteHarvestingClientCommand(dvRequestService.getDataverseRequest(), victim));
-            HarvestingClient merged = em.merge(victim);
-
-            // if this was a scheduled harvester, make sure the timer is deleted:
-            dataverseTimerService.removeHarvestTimer(victim);
-
-            // purge indexed objects:
-            indexService.deleteHarvestedDocuments(victim);
-            // All the datasets harvested by this client will be cleanly deleted 
-            // through the defined cascade. Cascaded delete does not work for harvested 
-            // files, however. So they need to be removed explicitly; before we 
-            // proceed removing the client itself. 
-            for (DataFile harvestedFile : dataFileService.findHarvestedFilesByClient(merged)) {
-                DataFile mergedFile = em.merge(harvestedFile);
-                em.remove(mergedFile);
-                harvestedFile = null;
-            }
-
-            em.remove(merged);
-        } catch (Exception e) {
-            errorMessage = "Failed to delete cleint. Unknown exception: " + e.getMessage();
-        }
-
-        if (errorMessage != null) {
-            logger.warning(errorMessage);
-        }
     }
 
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
