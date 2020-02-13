cf1f5ea0dbd5032bf27c2b5298da59be34e56899
MrK191 <karol191pol@gmail.com>
2020-02-13 17:08:43 +0100

    fixed displaying datasetfield differences again.

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/difference/DatasetFieldDiff.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/difference/DatasetFieldDiff.java
@@ -2,14 +2,17 @@ package edu.harvard.iq.dataverse.dataset.difference;
 
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetField;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetFieldType;
+import edu.harvard.iq.dataverse.persistence.dataset.DatasetFieldUtil;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetVersion;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
 
 import java.util.List;
 
 /**
  * Class that contains old and new value of {@link DatasetField}
  * that is different between two {@link DatasetVersion}s
- * 
+ *
  * @author madryk
  */
 public class DatasetFieldDiff extends MultipleItemDiff<DatasetField> {
@@ -17,7 +20,7 @@ public class DatasetFieldDiff extends MultipleItemDiff<DatasetField> {
     private DatasetFieldType fieldType;
 
     // -------------------- CONSTRUCTORS --------------------
-    
+
     public DatasetFieldDiff(List<DatasetField> oldValue, List<DatasetField> newValue, DatasetFieldType fieldType) {
         super(oldValue, newValue);
         this.fieldType = fieldType;
@@ -27,5 +30,14 @@ public class DatasetFieldDiff extends MultipleItemDiff<DatasetField> {
 
     public DatasetFieldType getFieldType() {
         return fieldType;
+    }
+
+    // -------------------- LOGIC --------------------
+
+    /**
+     * Generates DatasetFields joined value pair's so you can compare old and new values.
+     */
+    public Tuple2<String, String> generatePairs() {
+        return Tuple.of(DatasetFieldUtil.joinAllValues(getOldValue()), DatasetFieldUtil.joinAllValues(getNewValue()));
     }
 }
