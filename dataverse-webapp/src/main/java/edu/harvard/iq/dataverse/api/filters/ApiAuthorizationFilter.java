b3110363b7fedbc3644beb93e9bb9164c824176f
Wojciech Trębicki <trebicki@icm.edu.pl>
2020-02-25 15:03:41 +0100

    #682: Improvements

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/api/filters/ApiAuthorizationFilter.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/api/filters/ApiAuthorizationFilter.java
@@ -15,7 +15,20 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.function.Consumer;
 
+/**
+ *  When one is using Dataverse REST Api we need to set user in session
+ * so that user data will be accessible to all the services.
+ *  This filter is only used when Api call is done and it tries to log
+ * user into the session if following conditions are all met:
+ * <ol>
+ * <li>user is not already set in the session (i.e. user is not the guest user)</li>
+ * <li>user token is passed in header <i>X-Dataverse-key</i> or in the <i>key</i> query parameter.</li>
+ * </ol>
+ *  If the user was logged in by the filter, after the service call is done, user's
+ * session is invalidated.
+ */
 public class ApiAuthorizationFilter implements Filter {
 
     private DataverseSession session;
@@ -35,9 +48,10 @@ public class ApiAuthorizationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        boolean loggedByToken = logUserByTokenIfNeeded(servletRequest);
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        FilterLogIn filterLogIn = logInUserByTokenIfNeeded(request);
         chain.doFilter(servletRequest, response);
-        logoutIfLoggedByToken(servletRequest, loggedByToken);
+        filterLogIn.logOut(request);
     }
 
     @Override
@@ -48,30 +62,39 @@ public class ApiAuthorizationFilter implements Filter {
 
     // -------------------- PRIVATE ---------------------
 
-    private boolean logUserByTokenIfNeeded(ServletRequest servletRequest) {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
+    private FilterLogIn logInUserByTokenIfNeeded(HttpServletRequest request) {
         if (GuestUser.get().equals(session.getUser())) {
             String token = getRequestApiKey(request);
             AuthenticatedUser user = authenticationService.lookupUser(token);
             if (user != null) {
                 user = userService.updateLastApiUseTime(user);
                 session.setUser(user);
-                return true;
+                return FilterLogIn.TOKEN_LOG_IN;
             }
         }
-        return false;
-    }
-
-    private void logoutIfLoggedByToken(ServletRequest servletRequest, boolean loggedByToken) {
-        if (loggedByToken) {
-            HttpServletRequest request = (HttpServletRequest) servletRequest;
-            request.getSession().invalidate();
-        }
+        return FilterLogIn.NONE;
     }
 
     private String getRequestApiKey(HttpServletRequest request) {
         String headerParamApiKey = request.getHeader("X-Dataverse-key");
         String queryParamApiKey = request.getParameter("key");
         return headerParamApiKey != null ? headerParamApiKey : queryParamApiKey;
+    }
+
+    // -------------------- INNER CLASSES ---------------------
+
+    private enum FilterLogIn {
+        NONE(r -> { }),
+        TOKEN_LOG_IN(r -> r.getSession().invalidate());
+
+        private final Consumer<HttpServletRequest> logout;
+
+        public void logOut(HttpServletRequest request) {
+            logout.accept(request);
+        }
+
+        FilterLogIn(Consumer<HttpServletRequest> logout) {
+            this.logout = logout;
+        }
     }
 }
