d9da25794cecd43569fdec42fbd178462c2309f2
chopeen <chopeen@gmail.com>
2020-02-26 15:37:43 +0100

    removed try/catch to show the test is actually failing;

--- dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/util/json/DatasetVersionDTOTest.java
+++ dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/util/json/DatasetVersionDTOTest.java
@@ -18,7 +18,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -51,50 +51,41 @@ public class DatasetVersionDTOTest {
     public void tearDown() {
     }
 
-
     @Test
-    public void testReadDataSet() {
-        try {
+    public void testReadDataSet() throws FileNotFoundException {
+        File file = new File("src/test/java/edu/harvard/iq/dataverse/util/json/JsonDatasetVersion.txt");
+        String text = new Scanner(file).useDelimiter("\\Z").next();
+        Gson gson = new Gson();
+        DatasetVersionDTO dto = gson.fromJson(text, DatasetVersionDTO.class);
 
-            File file = new File("src/test/java/edu/harvard/iq/dataverse/util/json/JsonDatasetVersion.txt");
-            String text = new Scanner(file).useDelimiter("\\Z").next();
-            Gson gson = new Gson();
-            DatasetVersionDTO dto = gson.fromJson(text, DatasetVersionDTO.class);
+        HashSet<FieldDTO> author1Fields = new HashSet<>();
 
-            HashSet<FieldDTO> author1Fields = new HashSet<>();
+        author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorAffiliation", "Top"));
+        author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorIdentifier", "ellenid"));
+        author1Fields.add(FieldDTO.createVocabFieldDTO("authorIdentifierScheme", "ORCID"));
+        author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorName", "Privileged, Pete"));
 
-            author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorAffiliation", "Top"));
-            author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorIdentifier", "ellenid"));
-            author1Fields.add(FieldDTO.createVocabFieldDTO("authorIdentifierScheme", "ORCID"));
-            author1Fields.add(FieldDTO.createPrimitiveFieldDTO("authorName", "Privileged, Pete"));
+        HashSet<FieldDTO> author2Fields = new HashSet<>();
 
-            HashSet<FieldDTO> author2Fields = new HashSet<>();
+        author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorAffiliation", "Bottom"));
+        author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorIdentifier", "audreyId"));
+        author2Fields.add(FieldDTO.createVocabFieldDTO("authorIdentifierScheme", "DAISY"));
+        author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorName", "Awesome, Audrey"));
 
-            author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorAffiliation", "Bottom"));
-            author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorIdentifier", "audreyId"));
-            author2Fields.add(FieldDTO.createVocabFieldDTO("authorIdentifierScheme", "DAISY"));
-            author2Fields.add(FieldDTO.createPrimitiveFieldDTO("authorName", "Awesome, Audrey"));
+        List<HashSet<FieldDTO>> authorList = new ArrayList<>();
+        authorList.add(author1Fields);
+        authorList.add(author2Fields);
+        FieldDTO expectedDTO = new FieldDTO();
+        expectedDTO.setTypeName("author");
+        expectedDTO.setMultipleCompound(authorList);
 
-            List<HashSet<FieldDTO>> authorList = new ArrayList<>();
-            authorList.add(author1Fields);
-            authorList.add(author2Fields);
-            FieldDTO expectedDTO = new FieldDTO();
-            expectedDTO.setTypeName("author");
-            expectedDTO.setMultipleCompound(authorList);
+        FieldDTO authorDTO = dto.getMetadataBlocks().get("citation").getFields().get(1);
 
-            FieldDTO authorDTO = dto.getMetadataBlocks().get("citation").getFields().get(1);
+        // write both dto's to json to compare them with gson parser
 
-            // write both dto's to json to compare them with gson parser
+        JsonElement expected = gson.toJsonTree(expectedDTO, FieldDTO.class);
+        JsonElement result = gson.toJsonTree(authorDTO);
 
-            JsonElement expected = gson.toJsonTree(expectedDTO, FieldDTO.class);
-            JsonElement result = gson.toJsonTree(authorDTO);
-
-            Assert.assertEquals(expected, result);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Assert.assertEquals(expected, result);
     }
-
-
 }
