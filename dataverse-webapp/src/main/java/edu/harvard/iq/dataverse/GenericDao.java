94a505360c4c4431087ab94f77e2cd92f169a404
MrK191 <karol191pol@gmail.com>
2020-03-03 15:18:11 +0100

    removed unused method in GenericDao

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/GenericDao.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/GenericDao.java
@@ -17,8 +17,4 @@ public class GenericDao {
     public <T> T merge(T entity) {
         return em.merge(entity);
     }
-
-    public <T> void save(T entity) {
-        em.persist(entity);
-    }
 }
