b26e4bb865fdebc6c2896c68339b5bc19847b3ee
Tomasz Blazejczyk <blazejc@icm.edu.pl>
2020-03-27 13:04:24 +0100

    Closes #808: Auto refreshing of dataset when it is in-review state is against wcag rule

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
@@ -59,6 +59,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
@@ -1338,5 +1339,11 @@ public class DatasetPage implements java.io.Serializable {
             FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, BundleUtil.getStringFromBundle("dataset.embargo.validate.min.failureMessage"), null);
             context.addMessage(toValidate.getClientId(context), message);
         }
+    }
+    
+    public void datasetUnlockedInfo() {
+    	if (dataset.getLatestVersion().isDraft() && !permissionsWrapper.canIssuePublishDatasetCommand(dataset)) {
+    		JsfHelper.addMessage(FacesMessage.SEVERITY_INFO, BundleUtil.getStringFromBundle("dataset.unlocked.info"), BundleUtil.getStringFromBundle("dataset.unlocked.info.details"));
+    	}
     }
 }
