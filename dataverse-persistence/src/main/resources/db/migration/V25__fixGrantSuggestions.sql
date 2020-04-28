180c17fa4b71d0388382465c3f862586e6058d57
karol <karol191pol@gmail.com>
2020-04-28 11:51:14 +0200

    added flyway fix regarding grant table.

---
+++ dataverse-persistence/src/main/resources/db/migration/V25__fixGrantSuggestions.sql
@@ -0,0 +1,6 @@
+ALTER TABLE IF EXISTS grantsuggestions RENAME TO grantsuggestion;
+ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN grantAgency TO grantagency;
+ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN grantAgencyAcronym TO grantagencyacronym;
+ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN fundingProgram TO fundingprogram;
+ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN foreignName TO suggestionname;
+ALTER TABLE IF EXISTS grantsuggestion RENAME COLUMN foreignNameLocale TO suggestionnamelocale;
