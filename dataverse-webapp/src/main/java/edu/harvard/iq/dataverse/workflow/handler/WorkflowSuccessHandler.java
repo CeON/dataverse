da6383daa6eab5cefe5b2b0ddc23bbab5bef7a15
karol <karol191pol@gmail.com>
2020-06-22 12:56:33 +0200

    added javadocs to workflow interfaces.

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/workflow/handler/WorkflowSuccessHandler.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/workflow/handler/WorkflowSuccessHandler.java
@@ -2,7 +2,13 @@ package edu.harvard.iq.dataverse.workflow.handler;
 
 import edu.harvard.iq.dataverse.workflow.WorkflowExecutionContext;
 
+/**
+ * Interface that is used after the workflow is successful, it was made to be an interface so it can be implemented
+ * in external repositories.
+ * <p>
+ * Currently all implementations are going to be used after workflow succeeds.
+ */
 public interface WorkflowSuccessHandler {
     
-    public void handleSuccess(WorkflowExecutionContext workflowExecutionContext);
+    void handleSuccess(WorkflowExecutionContext workflowExecutionContext);
 }
