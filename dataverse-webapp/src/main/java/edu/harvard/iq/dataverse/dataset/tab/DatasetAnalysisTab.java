b6acef271f5fe52c7af4b6bdeea71daf12a64bbe
Tomasz Blazejczyk <blazejc@icm.edu.pl>
2020-07-01 17:32:27 +0200

    Closes #1172: Display results of automatic analysis with XDS

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/tab/DatasetAnalysisTab.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/dataset/tab/DatasetAnalysisTab.java
@@ -1,15 +1,20 @@
 package edu.harvard.iq.dataverse.dataset.tab;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import edu.harvard.iq.dataverse.DatasetFieldServiceBean;
+import edu.harvard.iq.dataverse.DataverseRequestServiceBean;
+import edu.harvard.iq.dataverse.PermissionsWrapper;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetField;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetFieldType;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetVersion;
+import edu.harvard.iq.dataverse.settings.SettingsServiceBean;
 
 @ViewScoped
 @Named("datasetAnalysisTab")
@@ -20,7 +25,22 @@ public class DatasetAnalysisTab implements Serializable {
     
     @EJB
     private DatasetFieldServiceBean datasetFields;
+    
+    private SettingsServiceBean settingsService;
+    
+    private PermissionsWrapper permissionsWrapper;
 
+    private DataverseRequestServiceBean dvRequestService;
+
+    @Inject
+    public DatasetAnalysisTab(SettingsServiceBean settingsService, PermissionsWrapper permissionsWrapper,
+            DataverseRequestServiceBean dvRequestService) {
+        this.settingsService = settingsService;
+        this.permissionsWrapper = permissionsWrapper;
+        this.dvRequestService = dvRequestService;
+
+
+    }
     
     // -------------------- GETTERS --------------------
 
@@ -57,9 +77,44 @@ public class DatasetAnalysisTab implements Serializable {
 
     
     public void init(DatasetVersion datasetVersion) {
-        
         this.datasetVersion = datasetVersion;
-        
+    }
+    
+    public boolean isDatasetUnderEmbargo() {
+        return datasetVersion.getDataset().hasActiveEmbargo();
+    }
+
+    public boolean isDatasetInDraft() {
+        return datasetVersion.isDraft();
+    }
+
+    public boolean isPermissionToViewFiles() {
+        return permissionsWrapper.canViewUnpublishedDataset(dvRequestService.getDataverseRequest(), datasetVersion.getDataset());
+    }
+
+    public boolean isAnalysisInProgress() {
+        return false;
+    }
+
+    public boolean isAnalysisPerformed() {
+        return false;
+    }
+
+    public boolean isAnalysisSucceeded() {
+        return false;
+    }
+
+    public boolean isAnalysisFailure() {
+        return false;
+    }
+
+    public boolean showAnalysisResults() {
+        return (!isDatasetUnderEmbargo() || (isDatasetUnderEmbargo() && isPermissionToViewFiles())) && isAnalysisSucceeded();
+    }
+
+    public String getEmbargoDateForDisplay() {
+        SimpleDateFormat format = new SimpleDateFormat(settingsService.getValueForKey(SettingsServiceBean.Key.DefaultDateFormat));
+        return format.format(datasetVersion.getDataset().getEmbargoDate().getOrNull());
     }
 
 }
