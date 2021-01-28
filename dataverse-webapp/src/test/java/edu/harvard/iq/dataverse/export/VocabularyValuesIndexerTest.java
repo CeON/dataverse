a5eb6aca0fd3dc3a816b568a061f9e4be23a14cd
Wojciech Trębicki <trebicki@icm.edu.pl>
2021-01-28 09:58:49 +0100

    Closes #1363: Improve test

--- dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/export/VocabularyValuesIndexerTest.java
+++ dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/export/VocabularyValuesIndexerTest.java
@@ -6,6 +6,7 @@ import edu.harvard.iq.dataverse.persistence.dataset.DatasetField;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetFieldType;
 import edu.harvard.iq.dataverse.persistence.dataset.DatasetVersion;
 import edu.harvard.iq.dataverse.persistence.dataset.FieldType;
+import edu.harvard.iq.dataverse.persistence.dataset.MetadataBlock;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -31,13 +32,13 @@ class VocabularyValuesIndexerTest {
     void indexTest() {
 
         // given
-        String vocabularyType1 = "vocabularyType1";
-        String vocabularyType2 = "vocabularyType2";
+        String vocabularyType1 = "collectionMode";
+        String vocabularyType2 = "dataSourceType";
 
         HashMap<String, List<String>> vocabularyData = new HashMap<>();
-        vocabularyData.put(vocabularyType1, Arrays.asList("value 1-1", "value 1-2", "value 1-3"));
-        vocabularyData.put(vocabularyType2, Arrays.asList("value 2-1", "value 2-2"));
-        DatasetVersion datasetVersion = createDatasetVersionWithGivenVocabularies(vocabularyData);
+        vocabularyData.put(vocabularyType1, Arrays.asList("focusgroup", "recording", "other"));
+        vocabularyData.put(vocabularyType2, Arrays.asList("processes.workflows", "eventsinteractions"));
+        DatasetVersion datasetVersion = createDatasetVersionWithGivenVocabularies("socialscience", vocabularyData);
 
         // when
         Map<String, Map<String, String>> index = indexer.indexLocalizedNamesOfUsedKeysByTypeAndValue(
@@ -47,23 +48,27 @@ class VocabularyValuesIndexerTest {
         assertThat(index.keySet()).containsExactlyInAnyOrder(vocabularyType1, vocabularyType2);
 
         assertThat(index.get(vocabularyType1)).hasSize(3);
-        assertThat(index.get(vocabularyType1).get("value 1-1")).isEqualTo("value 1-1");
-        assertThat(index.get(vocabularyType1).get("value 1-2")).isEqualTo("value 1-2");
-        assertThat(index.get(vocabularyType1).get("value 1-3")).isEqualTo("value 1-3");
+        assertThat(index.get(vocabularyType1).get("focusgroup")).isEqualTo("Focus group");
+        assertThat(index.get(vocabularyType1).get("recording")).isEqualTo("Recording");
+        assertThat(index.get(vocabularyType1).get("other")).isEqualTo("Other");
 
         assertThat(index.get(vocabularyType2)).hasSize(2);
-        assertThat(index.get(vocabularyType2).get("value 2-1")).isEqualTo("value 2-1");
-        assertThat(index.get(vocabularyType2).get("value 2-2")).isEqualTo("value 2-2");
+        assertThat(index.get(vocabularyType2).get("processes.workflows")).isEqualTo("Processes: Workflow(s)");
+        assertThat(index.get(vocabularyType2).get("eventsinteractions")).isEqualTo("Events/Interactions");
     }
 
     // -------------------- PRIVATE --------------------
 
-    private DatasetVersion createDatasetVersionWithGivenVocabularies(Map<String, List<String>> vocabularyData) {
+    private DatasetVersion createDatasetVersionWithGivenVocabularies(String metadataBlockName,
+                                                                     Map<String, List<String>> vocabularyData) {
         DatasetVersion datasetVersion = new DatasetVersion();
+        MetadataBlock metadataBlock = new MetadataBlock();
+        metadataBlock.setName(metadataBlockName);
         Random idGenerator = new Random();
 
         for (Map.Entry<String, List<String>> entry : vocabularyData.entrySet()) {
             DatasetFieldType type = new DatasetFieldType(entry.getKey(), FieldType.TEXT, true);
+            type.setMetadataBlock(metadataBlock);
             Set<ControlledVocabularyValue> values = entry.getValue().stream()
                     .map(v -> new ControlledVocabularyValue(Math.abs(idGenerator.nextLong()), v, type))
                     .collect(Collectors.toSet());
