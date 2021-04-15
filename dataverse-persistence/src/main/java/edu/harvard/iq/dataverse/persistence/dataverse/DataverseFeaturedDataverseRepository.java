3aa68cd4aafc8c4131dcfb8bb51b46a4c9b5815d
Krzysztof Mądry <madryk@icm.edu.pl>
2021-04-15 11:00:44 +0200

    Closes #1535: Fix memory issues on dataverse page

---
+++ dataverse-persistence/src/main/java/edu/harvard/iq/dataverse/persistence/dataverse/DataverseFeaturedDataverseRepository.java
@@ -0,0 +1,25 @@
+package edu.harvard.iq.dataverse.persistence.dataverse;
+
+import edu.harvard.iq.dataverse.persistence.JpaRepository;
+
+import javax.ejb.Stateless;
+
+import java.util.List;
+
+@Stateless
+public class DataverseFeaturedDataverseRepository extends JpaRepository<Long, DataverseFeaturedDataverse> {
+
+    // -------------------- CONSTRUCTORS --------------------
+
+    public DataverseFeaturedDataverseRepository() {
+        super(DataverseFeaturedDataverse.class);
+    }
+
+    public List<DataverseFeaturedDataverse> findByDataverseId(Long dataverseId) {
+        String query = "select object(o) from DataverseFeaturedDataverse as o where o.dataverse.id = :dataverseId order by o.displayOrder";
+        return em.createQuery(query, DataverseFeaturedDataverse.class)
+                .setParameter("dataverseId", dataverseId)
+                .getResultList();
+    }
+
+}
