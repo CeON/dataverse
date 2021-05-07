67b8aa9a7b54499a22a1af52a6b1ab8863254ac0
dbojanek <dbojanek@icm.edu.pl>
2021-05-07 18:34:33 +0200

    Closes #1573: ChangeUserIdentifier endpoint

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/api/Users.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/api/Users.java
@@ -1,10 +1,10 @@
 package edu.harvard.iq.dataverse.api;
 
-import edu.harvard.iq.dataverse.engine.command.impl.ChangeUserIdentifierCommand;
-import edu.harvard.iq.dataverse.persistence.user.AuthenticatedUser;
 import edu.harvard.iq.dataverse.persistence.user.User;
+import edu.harvard.iq.dataverse.users.ChangeUserIdentifierService;
 
-import javax.ejb.Stateless;
+import javax.ejb.EJBException;
+import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,43 +14,34 @@ import javax.ws.rs.core.Response;
  *
  * @author madunlap
  */
-@Stateless
 @Path("users")
 public class Users extends AbstractApiBean {
+
+    @Inject
+    private ChangeUserIdentifierService changeUserIdentifierService;
 
     @POST
     @Path("{identifier}/changeIdentifier/{newIdentifier}")
     public Response changeAuthenticatedUserIdentifier(@PathParam("identifier") String oldIdentifier, @PathParam("newIdentifier")  String newIdentifier) {
-        User user;
         try {
+            User user;
             user = findUserOrDie();
-            if(!user.isSuperuser()) {
-                throw new WrappedResponse(error(Response.Status.UNAUTHORIZED, "Only superusers can change userIdentifiers"));
-            }
+            changeUserIdentifierService.changeUserIdentifier(user, oldIdentifier, newIdentifier);
+
+            return ok("UserIdentifier changed from " + oldIdentifier + " to " + newIdentifier);
         } catch (WrappedResponse ex) {
             return ex.getResponse();
-        }
-
-        if(oldIdentifier == null || oldIdentifier.isEmpty()) {
-            return error(Response.Status.BAD_REQUEST, "Old identifier provided to change is empty.");
-        } else if(newIdentifier == null || newIdentifier.isEmpty()) {
-            return error(Response.Status.BAD_REQUEST, "New identifier provided to change is empty.");
-        } else if(newIdentifier.equals(oldIdentifier)) {
-            return error(Response.Status.BAD_REQUEST, "New identifier must differ from the old");
-        }
-
-        AuthenticatedUser authenticatedUser = authSvc.getAuthenticatedUser(oldIdentifier);
-        if (authenticatedUser == null) {
-            return error(Response.Status.BAD_REQUEST, "User " + oldIdentifier + " not found in AuthenticatedUser");
-        }
-
-        try {
-            execCommand(new ChangeUserIdentifierCommand(createDataverseRequest(user), authenticatedUser,  newIdentifier));
+        } catch (SecurityException se) {
+            return error(Response.Status.UNAUTHORIZED, se.getLocalizedMessage());
+        } catch (IllegalArgumentException iae) {
+            return error(Response.Status.EXPECTATION_FAILED, iae.getLocalizedMessage());
+        } catch (IllegalStateException ise) {
+            return error(Response.Status.EXPECTATION_FAILED, ise.getLocalizedMessage());
+        } catch (EJBException ise) {
+            return badRequest(ise.getCause().getMessage());
         } catch (Exception e){
             return error(Response.Status.BAD_REQUEST, "Error calling ChangeUserIdentifierCommand: " + e.getLocalizedMessage());
         }
-
-        return ok("UserIdentifier changed from " + oldIdentifier + " to " + newIdentifier);
     }
 
 }
