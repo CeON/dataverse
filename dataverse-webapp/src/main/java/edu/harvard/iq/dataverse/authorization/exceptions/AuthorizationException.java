f09de65157cc1d9160999763064852a331c7ca0a
Wojciech Trębicki <trebicki@icm.edu.pl>
2021-05-14 09:16:35 +0200

    Closes #1547: Fix

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/authorization/exceptions/AuthorizationException.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/authorization/exceptions/AuthorizationException.java
@@ -1,14 +1,11 @@
 package edu.harvard.iq.dataverse.authorization.exceptions;
 
-import javax.ejb.ApplicationException;
-
 /**
  * Base class for exceptions thrown by the authorization package classes.
  *
  * @author michael
  */
 
-@ApplicationException
 public class AuthorizationException extends Exception {
 
     public AuthorizationException(String message) {
