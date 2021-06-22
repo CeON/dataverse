b45dea7ee10465e30503f94f3270ff12e86859e5
MrK191 <karol191pol@gmail.com>
2021-06-22 13:31:04 +0200

    fixed test

--- dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/search/ror/RorIndexingServiceTest.java
+++ dataverse-webapp/src/test/java/edu/harvard/iq/dataverse/search/ror/RorIndexingServiceTest.java
@@ -3,8 +3,6 @@ package edu.harvard.iq.dataverse.search.ror;
 import com.google.common.collect.ImmutableList;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.common.SolrInputDocument;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -40,25 +38,8 @@ class RorIndexingServiceTest {
         rorIndexingService.indexRorRecord(rorData);
 
         //then
-        Mockito.verify(solrClient, Mockito.times(1)).add(Mockito.any(SolrInputDocument.class));
+        Mockito.verify(solrClient, Mockito.times(1)).addBean(rorData);
         Mockito.verify(solrClient, Mockito.times(1)).commit();
 
-    }
-
-    @Test
-    void indexRorRecord_withoutId() {
-        //given
-        String rorId = "testRor";
-        String name = "testName";
-        String countryName = "Poland";
-        String countryCode = "PL";
-        final ImmutableList<String> aliases = ImmutableList.of("alias");
-        final ImmutableList<String> acronyms = ImmutableList.of("acronym");
-        final ImmutableList<String> labels = ImmutableList.of("label");
-
-        final RorDto rorData = new RorDto(rorId, name, countryName, countryCode, "","", aliases, acronyms, labels);
-
-        //when & then
-        Assertions.assertThatThrownBy(() -> rorIndexingService.indexRorRecord(rorData)).isInstanceOf(AssertionError.class);
     }
 }
