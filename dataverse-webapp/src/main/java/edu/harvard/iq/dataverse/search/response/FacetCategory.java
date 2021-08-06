bc9cf4c957e57a909b52aaee5022f2ee55c56452
Krzysztof Mądry <madryk@icm.edu.pl>
2021-08-06 17:34:09 +0200

    Closes #1867: Fix issue with fetching datasetfieldtypes multiple times on search page

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/search/response/FacetCategory.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/search/response/FacetCategory.java
@@ -1,11 +1,12 @@
 package edu.harvard.iq.dataverse.search.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FacetCategory {
 
     private String name;
-    private List<FacetLabel> facetLabel;
+    private List<FacetLabel> facetLabel = new ArrayList<>(); // TODO: should be plural
     private String friendlyName;
 
     public String getName() {
@@ -14,6 +15,10 @@ public class FacetCategory {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void addSingleFacetLabel(FacetLabel facetLabel) {
+        this.facetLabel.add(facetLabel);
     }
 
     public List<FacetLabel> getFacetLabel() {
