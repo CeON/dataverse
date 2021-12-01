bd05cb86f50108057342fcf57db3efffaac52193
Wojciech Trębicki <trebicki@icm.edu.pl>
2021-12-01 08:57:16 +0100

    Closes #2082, #2083: CR fixes 2

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/api/imports/ImportServiceBean.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/api/imports/ImportServiceBean.java
@@ -5,7 +5,6 @@ import com.google.gson.GsonBuilder;
 import edu.harvard.iq.dataverse.DatasetDao;
 import edu.harvard.iq.dataverse.EjbDataverseEngine;
 import edu.harvard.iq.dataverse.api.dto.DatasetDTO;
-import edu.harvard.iq.dataverse.api.imports.ImportUtil.ImportType;
 import edu.harvard.iq.dataverse.engine.command.DataverseRequest;
 import edu.harvard.iq.dataverse.engine.command.exception.CommandException;
 import edu.harvard.iq.dataverse.engine.command.impl.CreateHarvestedDatasetCommand;
