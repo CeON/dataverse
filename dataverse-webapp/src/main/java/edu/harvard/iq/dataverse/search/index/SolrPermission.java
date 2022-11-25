af9f585a44c4185b966b930bdc7933552efd5acf
Wojciech Trębicki <trebicki@icm.edu.pl>
2022-11-25 16:16:43 +0100

    Closes #2230: Add AddDataset permission info into solr

---
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/search/index/SolrPermission.java
@@ -0,0 +1,27 @@
+package edu.harvard.iq.dataverse.search.index;
+
+import edu.harvard.iq.dataverse.persistence.user.Permission;
+
+import java.util.List;
+
+public class SolrPermission {
+    private List<String> permittedEntities;
+    private Permission type;
+
+    // -------------------- CONSTRUCTORS --------------------
+
+    public SolrPermission(Permission type, List<String> permittedEntities) {
+        this.permittedEntities = permittedEntities;
+        this.type = type;
+    }
+
+    // -------------------- GETTERS --------------------
+
+    public List<String> getPermittedEntities() {
+        return permittedEntities;
+    }
+
+    public Permission getType() {
+        return type;
+    }
+}
