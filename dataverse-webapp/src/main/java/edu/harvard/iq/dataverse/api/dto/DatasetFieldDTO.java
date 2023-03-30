f204fd3eb5c2b2d5de263eee1511f13fac4e6bb1
Wojciech Trębicki <trebicki@icm.edu.pl>
2023-03-30 14:02:52 +0200

    Closes #2340: Fix NPE when exporting dataset with empty vocabulary field

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/api/dto/DatasetFieldDTO.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/api/dto/DatasetFieldDTO.java
@@ -225,6 +225,9 @@ public class DatasetFieldDTO {
                             .map(this::extractChildren)
                             .collect(Collectors.toList());
                 }
+                if (values.isEmpty()) {
+                    continue;
+                }
                 field.setValue(extractValue(fieldType, values));
                 fields.add(field);
             }
