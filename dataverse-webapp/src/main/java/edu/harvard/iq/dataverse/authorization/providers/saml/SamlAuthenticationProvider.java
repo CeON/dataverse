1bdcdac9395d3f2575267d3bb1e0165848dd6a59
Wojciech Trębicki <trebicki@icm.edu.pl>
2023-04-19 12:54:32 +0200

    Closes #2356: Change SAML button text

--- dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/authorization/providers/saml/SamlAuthenticationProvider.java
+++ dataverse-webapp/src/main/java/edu/harvard/iq/dataverse/authorization/providers/saml/SamlAuthenticationProvider.java
@@ -5,6 +5,7 @@ import com.onelogin.saml2.authn.AuthnRequestParams;
 import com.onelogin.saml2.exception.SettingsException;
 import edu.harvard.iq.dataverse.authorization.AuthenticationProvider;
 import edu.harvard.iq.dataverse.authorization.AuthenticationProviderDisplayInfo;
+import edu.harvard.iq.dataverse.common.BundleUtil;
 import edu.harvard.iq.dataverse.persistence.user.SamlIdentityProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,7 +54,7 @@ public class SamlAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public AuthenticationProviderDisplayInfo getInfo() {
-        return new AuthenticationProviderDisplayInfo(getId(), "SAML", "SAML Authorization");
+        return new AuthenticationProviderDisplayInfo(getId(), BundleUtil.getStringFromBundle("saml.login.btn"), "SAML Authorization");
     }
 
     public List<SamlIdentityProvider> getRegisteredProviders() {
