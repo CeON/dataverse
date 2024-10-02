8a16671c70db5b22e13c33309df88aef05dd0156
lbownik <lbownik@icm.edu.pl>
2024-10-02 15:02:42 +0200

    replaced
    public static String format(String format,
                                Object... args)
    
    with
    
    public static String format(Locale l,
                                String format,
                                Object... args)
    
    so that the same locale be used for both byte abbreviation and decimal separator

--- dataverse-persistence/src/main/java/edu/harvard/iq/dataverse/common/FileSizeUtil.java
+++ dataverse-persistence/src/main/java/edu/harvard/iq/dataverse/common/FileSizeUtil.java
@@ -1,24 +1,38 @@
 package edu.harvard.iq.dataverse.common;
 
-public class FileSizeUtil {
-    
-    /* This method turns a number of bytes into a human readable version
-     */
-    public static String bytesToHumanReadable(long v) {
-        return bytesToHumanReadable(v, 1);
-    }
+import static edu.harvard.iq.dataverse.common.BundleUtil.getCurrentLocale;
+import static edu.harvard.iq.dataverse.common.BundleUtil.getStringFromBundle;
+import static java.lang.Long.numberOfLeadingZeros;
+import static java.lang.String.format;
 
-    /* This method turns a number of bytes into a human readable version
-     * with figs decimal places
-     */
-    public static String bytesToHumanReadable(long v, int figs) {
-        if (v < 1024) {
-            return v + " " + BundleUtil.getStringFromBundle("file.addreplace.error.byte_abrev");
-        }
-        // 63 - because long has 63 binary digits
-        int trailingBin0s = (63 - Long.numberOfLeadingZeros(v)) / 10;
-        //String base = "%."+figs+"f %s"+ BundleUtil.getStringFromBundle("file.addreplace.error.byte_abrev");
-        return String.format("%." + figs + "f %s" + BundleUtil.getStringFromBundle("file.addreplace.error.byte_abrev"), (double) v / (1L << (trailingBin0s * 10)),
-                             " KMGTPE".charAt(trailingBin0s));
-    }
+public class FileSizeUtil {
+
+	/*
+	 * This method turns a number of bytes into a human readable version
+	 */
+	public static String bytesToHumanReadable(final long v) {
+
+		return bytesToHumanReadable(v, 1);
+	}
+
+	/*
+	 * This method turns a number of bytes into a human readable version with figs
+	 * decimal places
+	 */
+	public static String bytesToHumanReadable(final long v, final int figs) {
+
+		final String B = getStringFromBundle("file.addreplace.error.byte_abrev");
+
+		if (v < 1024) {
+			return v + " " + B;
+		} else {
+			final String formatStr = "%." + figs + "f %s" + B;
+			// 63 - because long has 63 binary digits
+			final int trailingBin0s = (63 - numberOfLeadingZeros(v)) / 10;
+			final char magnitude = " KMGTPE".charAt(trailingBin0s);
+			final double roundedValue = (double) v / (1L << (trailingBin0s * 10));
+			
+			return format(getCurrentLocale(), formatStr, roundedValue, magnitude);
+		}
+	}
 }
